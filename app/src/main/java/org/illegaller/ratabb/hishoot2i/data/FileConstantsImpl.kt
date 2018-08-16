01c5cafce2d2e2f52750e252eca04f3786f544ec
ratabb <ratabilabagi@gmail.com>
2018-08-17 02:45:37 +0700

    Handle device without external storage

--- app/src/main/java/org/illegaller/ratabb/hishoot2i/data/FileConstantsImpl.kt
+++ app/src/main/java/org/illegaller/ratabb/hishoot2i/data/FileConstantsImpl.kt
@@ -3,52 +3,43 @@ package org.illegaller.ratabb.hishoot2i.data
 import android.content.Context
 import android.net.Uri
 import android.os.Environment
-import android.support.v4.content.FileProvider
-import org.illegaller.ratabb.hishoot2i.BuildConfig
-import rbb.hishoot2i.common.FileConstants
+import android.support.v4.content.FileProvider.getUriForFile
+import org.illegaller.ratabb.hishoot2i.BuildConfig.FILE_AUTHORITY
 import java.io.File
 import java.io.IOException
 import javax.inject.Inject
 
-class FileConstantsImpl @Inject constructor(val context: Context) : FileConstants {
-    private var saved: File? = null
-    private var htz: File? = null
-    private var bgCrop: File? = null
+class FileConstantsImpl @Inject constructor(val context: Context) : common.FileConstants {
     override fun savedDir(): File {
-        saved?.let { return it }
-        val result = File(Environment.getExternalStorageDirectory(), "HiShoot")
-        if (!result.exists()) {
-            result.mkdirs()
+        val parent = Environment.getExternalStorageState().let { state ->
+            when (state) {
+                Environment.MEDIA_MOUNTED -> Environment.getExternalStorageDirectory()
+                else -> context.getExternalFilesDir(null)
+            }.let { if (!it.canWrite()) context.filesDir else it }
         }
-        saved = result
+        val result = File(parent, "HiShoot")
+        if (!result.exists()) result.mkdirs()
         return result
     }
 
     override fun htzDir(): File {
-        htz?.let { return it }
         val result = File(context.filesDir, "htz")
-        if (!result.exists()) {
-            result.mkdirs()
-        }
+        if (!result.exists()) result.mkdirs()
         try {
-            File(result, FileConstants.NO_MEDIA).createNewFile()
-        } catch (e: IOException) {
-            e.printStackTrace()
+            File(result, common.FileConstants.NO_MEDIA).createNewFile()
+        } catch (ignore: IOException) {
         }
-        htz = result
         return result
     }
 
-    override fun File.toUri(): Uri =
-        FileProvider.getUriForFile(context, BuildConfig.FILE_AUTHORITY, this)
-
+    override fun File.toUri(): Uri = getUriForFile(context, FILE_AUTHORITY, this)
     override fun bgCrop(): File {
-        bgCrop?.let { return it }
-        val result =
-            File(context.cacheDir, FileConstants.BG_CROP)
+        val result = File(context.cacheDir, common.FileConstants.BG_CROP)
         if (result.exists()) result.delete()
-        result.createNewFile()
-        bgCrop = result
+        try {
+            result.createNewFile()
+        } catch (e: IOException) {
+        }
         return result
     }
 }
