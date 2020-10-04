45bfaffbdc518d1af69cec3ea11d173d432746c8
ratabb <ratabilabagi@gmail.com>
2020-10-04 11:27:49 +0700

    add centralize constants ui

---
+++ app/src/main/java/org/illegaller/ratabb/hishoot2i/ui/constants.kt
@@ -0,0 +1,21 @@
+@file:Suppress("SpellCheckingInspection")
+
+package org.illegaller.ratabb.hishoot2i.ui
+
+const val ARG_BACKGROUND_PATH = "arg_background_path"
+const val ARG_COLOR = "arg_color"
+const val ARG_CROP_PATH = "arg_crop_path"
+const val ARG_PIPETTE_COLOR = "arg_pipette_color"
+const val ARG_SCREEN1_PATH = "arg_screen1_path"
+const val ARG_SCREEN2_PATH = "arg_screen2_path"
+const val ARG_SORT = "arg_sort"
+const val ARG_THEME = "arg_theme"
+
+const val KEY_REQ_BACKGROUND = "key_background_request"
+const val KEY_REQ_CROP = "key_crop_request"
+const val KEY_REQ_MIX_COLOR = "key_mix_color_request"
+const val KEY_REQ_PIPETTE = "key_pipette_request"
+const val KEY_REQ_SCREEN_1 = "key_screen1_request"
+const val KEY_REQ_SCREEN_2 = "key_screen2_request"
+const val KEY_REQ_SORT = "key_sort_request"
+const val KEY_REQ_THEME = "key_theme_request"
