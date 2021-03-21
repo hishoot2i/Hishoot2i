0f3a7f2f464fb5057e6ae74497f9af1a83a2fbb5
ratabb <ratabilabagi@gmail.com>
2021-03-21 12:32:43 +0700

    fix(common) improve creating Typeface

--- common/src/main/java/common/ext/graphics/Typeface.kt
+++ common/src/main/java/common/ext/graphics/Typeface.kt
@@ -3,9 +3,17 @@
 package common.ext.graphics
 
 import android.graphics.Typeface
+import java.io.File
 
-inline fun String.createFromFileOrDefault(): Typeface = try {
-    Typeface.createFromFile(this)
-} catch (e: Exception) {
-    Typeface.DEFAULT
+const val DEFAULT_TYPEFACE_KEY = "DEFAULT"
+
+inline fun String?.typeFaceOrDefault(): Typeface = when (this) {
+    null, DEFAULT_TYPEFACE_KEY -> Typeface.DEFAULT
+    else -> File(this).takeIf(File::canRead)?.let { file ->
+        try { // only try create typeface if file is can read.
+            Typeface.createFromFile(file)
+        } catch (_: Exception) {
+            Typeface.DEFAULT
+        }
+    } ?: Typeface.DEFAULT
 }
