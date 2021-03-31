2fcbbd1694597178007149279c2dd29838a13a79
ratabb <ratabilabagi@gmail.com>
2021-03-31 19:19:15 +0700

    fix(common) remove File ext

--- app/src/main/java/org/illegaller/ratabb/hishoot2i/data/resolver/HtzResolverImpl.kt
+++ app/src/main/java/org/illegaller/ratabb/hishoot2i/data/resolver/HtzResolverImpl.kt
@@ -1,14 +1,15 @@
 package org.illegaller.ratabb.hishoot2i.data.resolver
 
 import common.FileConstants
-import common.ext.isDirAndCanRead
-import common.ext.listFilesOrEmpty
 import java.io.File
 import javax.inject.Inject
 
 class HtzResolverImpl @Inject constructor(
     private val fileConstants: FileConstants
 ) : HtzResolver {
-    override fun installedHtz(): List<File> = fileConstants.htzDir()
-        .listFilesOrEmpty(File::isDirAndCanRead).toList()
+    override fun installedHtz(): List<File> = (
+        fileConstants.htzDir().listFiles { child ->
+            child.isDirectory && child.canRead()
+        } ?: emptyArray()
+        ).toList()
 }
--- app/src/main/java/org/illegaller/ratabb/hishoot2i/data/source/FileFontSourceImpl.kt
+++ app/src/main/java/org/illegaller/ratabb/hishoot2i/data/source/FileFontSourceImpl.kt
@@ -2,8 +2,6 @@ package org.illegaller.ratabb.hishoot2i.data.source
 
 import android.os.Environment
 import common.ext.graphics.DEFAULT_TYPEFACE_KEY
-import common.ext.isDirAndCanRead
-import common.ext.listFilesByExt
 import org.illegaller.ratabb.hishoot2i.data.pref.SettingPref
 import java.io.File
 import javax.inject.Inject
@@ -17,11 +15,15 @@ class FileFontSourceImpl @Inject constructor(
 
     private fun fileFonts(): List<File> =
         (defaultFontDir() + customFontDir() + systemFontDir()).filterNotNull()
-            .filter(File::isDirAndCanRead)
-            .map(::fontsByExt)
-            .flatMap(Array<File>::asList)
-            .distinctBy(File::nameWithoutExtension)
-            .sortedBy(File::getName)
+            .filter { it.isDirectory && it.canRead() }
+            .map { file ->
+                file.listFiles { child ->
+                    child.extension in arrayOf("ttf", "otf")
+                } ?: emptyArray()
+            }
+            .flatMap { it.asList() }
+            .distinctBy { it.nameWithoutExtension }
+            .sortedBy { it.name }
 
     private fun defaultFontDir(): List<File> =
         arrayOf("font", "fonts", "Font", "Fonts").map(::externalStorageDir)
@@ -30,8 +32,6 @@ class FileFontSourceImpl @Inject constructor(
 
     private fun systemFontDir(): File? =
         if (settingPref.systemFontEnable) File("/system/fonts") else null
-
-    private fun fontsByExt(file: File): Array<File> = file.listFilesByExt("ttf", "otf")
 
     @Suppress("DEPRECATION")
     private fun externalStorageDir(path: String) =
