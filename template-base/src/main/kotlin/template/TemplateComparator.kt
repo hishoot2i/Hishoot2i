20bf044d1b1c51170a49b0f3052b71d72ab8b85d
ratabb <ratabilabagi@gmail.com>
2021-04-10 00:33:38 +0700

    fix(template-base) rewrite TemplateComparator.kt

--- template-base/src/main/kotlin/template/TemplateComparator.kt
+++ template-base/src/main/kotlin/template/TemplateComparator.kt
@@ -7,55 +7,36 @@ import template.Template.Version3
 import template.Template.VersionHtz
 import java.text.Collator
 
+private val Template.typeSort: Byte
+    get() = when (this) {
+        is Default -> 0x0
+        is Version1 -> 0x1
+        is Version2 -> 0x2
+        is Version3 -> 0x3
+        is VersionHtz -> 0x4
+    }
+
 enum class TemplateComparator : Comparator<Template> {
-    NAME_ASC {
-        override val impl: (Template, Template) -> Int = { lhs, rhs ->
-            collator.compare(lhs.name, rhs.name)
-        }
-    },
-    NAME_DESC {
-        override val impl: (Template, Template) -> Int = { lhs, rhs ->
-            collator.compare(rhs.name, lhs.name)
-        }
-    },
-    TYPE_ASC {
-        override val impl: (Template, Template) -> Int = { lhs, rhs ->
-            lhs.typeSort.compareTo(rhs.typeSort).takeIf { it != 0 }
-                ?: NAME_ASC.impl(lhs, rhs)
-        }
-    },
-    TYPE_DESC {
-        override val impl: (Template, Template) -> Int = { lhs, rhs ->
-            rhs.typeSort.compareTo(lhs.typeSort).takeIf { it != 0 }
-                ?: NAME_ASC.impl(lhs, rhs)
-        }
-    },
-    DATE_ASC {
-        override val impl: (Template, Template) -> Int = { lhs, rhs ->
-            lhs.installedDate.compareTo(rhs.installedDate).takeIf { it != 0 }
-                ?: NAME_ASC.impl(lhs, rhs)
-        }
-    },
-    DATE_DESC {
-        override val impl: (Template, Template) -> Int = { lhs, rhs ->
-            rhs.installedDate.compareTo(lhs.installedDate).takeIf { it != 0 }
-                ?: NAME_ASC.impl(lhs, rhs)
-        }
-    };
+    NAME_ASC, NAME_DESC, TYPE_ASC, TYPE_DESC, DATE_ASC, DATE_DESC; //
 
-    protected abstract val impl: (Template, Template) -> Int
-
-    protected val collator: Collator by lazy { Collator.getInstance() }
-
-    protected val Template.typeSort
-        get() = when (this) {
-            is Default -> 0
-            is Version1 -> 1
-            is Version2 -> 2
-            is Version3 -> 3
-            is VersionHtz -> 4
-        }
+    companion object {
+        private val collator = Collator.getInstance()
+    }
 
     override fun compare(lhs: Template, rhs: Template): Int =
-        if (lhs is Default || rhs is Default) 1 else impl(lhs, rhs)
+        if (lhs is Default || rhs is Default) 1 // Template Default always on top
+        else when (this) {
+            NAME_ASC -> compareValuesBy(lhs, rhs, collator, Template::name)
+                .takeIf { it != 0 } ?: compareValuesBy(lhs, rhs, collator, Template::author)
+            NAME_DESC -> compareValuesBy(rhs, lhs, collator, Template::name)
+                .takeIf { it != 0 } ?: compareValuesBy(lhs, rhs, collator, Template::author)
+            TYPE_ASC -> compareValuesBy(lhs, rhs, Template::typeSort)
+                .takeIf { it != 0 } ?: NAME_ASC.compare(lhs, rhs)
+            TYPE_DESC -> compareValuesBy(rhs, lhs, Template::typeSort)
+                .takeIf { it != 0 } ?: NAME_ASC.compare(lhs, rhs)
+            DATE_ASC -> compareValuesBy(lhs, rhs, Template::installedDate)
+                .takeIf { it != 0 } ?: NAME_ASC.compare(lhs, rhs)
+            DATE_DESC -> compareValuesBy(rhs, lhs, Template::installedDate)
+                .takeIf { it != 0 } ?: NAME_ASC.compare(lhs, rhs)
+        }
 }
